80df1a6e4356aed3f3def83db927c6ddb79ac556
S <stevenxsx@gmail.com>
2022-03-04 08:07:20 +0100

    Work begun

--- src/Card.java
+++ src/Card.java
@@ -2,11 +2,13 @@ public class Card {
 
     private final Suit suit;
     private final int value;
+    private boolean faceUp;
 
 
     public Card(Suit suit, int value) {
         this.suit = suit;
         this.value = value;
+        faceUp = false;
     }
 
 
--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,7 @@ public class Game {
 
 
     public void startGame() {
-
+        //make sure cards are shuffled
+        //load cards into game space
     }
 }
