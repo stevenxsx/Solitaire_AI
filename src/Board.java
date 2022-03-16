9f41e8b10302ebe237f09c1572fabf624a005dbe
Simon James Jensen <acehole_42@hotmail.com>
2022-03-16 13:52:11 +0100

    Grab aces AI function

--- src/Board.java
+++ src/Board.java
@@ -32,7 +32,7 @@ public class Board {
     }
     /** AI functions
      All functions must find all candidate cards that apply, then transfer the candidate from the pile with the most downcards
-     Search for Ace in number piles and move to foundation
+     SIMON -- Search for Ace in number piles and move to foundation
      Search for Deuce in number piles and move to foundation
      Search for transferable face-up card(s) that will free a face-down card
          If search returns multiple options select pile with most face-down cards
